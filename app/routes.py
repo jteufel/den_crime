b66b9cef23d3350ba7d971c70350aa752db2b1c3
jteufel <36942884+jteufel@users.noreply.github.com>
2020-05-01 14:28:16 -0600

    Final for CSPB 3287

--- app/routes.py
+++ app/routes.py
@@ -1,18 +1,15 @@
 from flask import render_template
 from app import app
+from db_query import *
 
 @app.route('/')
 @app.route('/index')
-def index():
-    user = {'username': 'Justin'}
-    posts = [
-        {
-            'author': {'username': 'John'},
-            'body': 'Beautiful day in Portland!'
-        },
-        {
-            'author': {'username': 'Susan'},
-            'body': 'The Avengers movie was so cool!'
-        }
-    ]
-    return render_template('index.html', title='Home', user=user, posts=posts)
+
+def index(): #from flask tutorial
+
+    conn = DB_Connect() #connect to database
+    json_data = Build_JSON(conn,"crime") #function to query database and store statisical data in dict
+    Write_JSON(json_data ,"data/crime.json") #write json to be read by .js file
+    DB_Close(conn)
+
+    return render_template('crime.html')
